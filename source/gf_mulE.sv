7a8b06f5b24e36e452ebc50acb31691567d1fc92
Mahesh Babu Gorantla <mgorantl@purdue.edu>
2017-03-13 07:33:52 -0400

    mix_columns.sv works as expected need to make sure that the input data format is consistent with other modules

---
+++ source/gf_mulE.sv
@@ -0,0 +1,26 @@
+// $Id: $
+// File name:   gf_mulE.sv
+// Created:     3/13/2017
+// Author:      Mahesh Babu Gorantla
+// Lab Section: 337-02
+// Version:     1.0  Initial Design Entry
+// Description: "This module is used to perform the multiplication with 'E' in Galois Field"
+
+module gf_mulE
+(
+	input wire [7:0] byte_in,
+	output wire [7:0] byte_out
+);
+	
+	assign byte_out = { 
+				byte_in[4] ^ byte_in[6] ^ byte_in[5],
+				byte_in[7] ^ byte_in[4] ^ byte_in[5] ^ byte_in[3],
+				byte_in[6] ^ byte_in[2] ^ byte_in[4] ^ byte_in[3],
+				byte_in[1] ^ byte_in[2] ^ byte_in[5] ^ byte_in[3],
+				byte_in[0] ^ byte_in[5] ^ byte_in[1] ^ byte_in[2] ^ byte_in[6],
+				byte_in[1] ^ byte_in[6] ^ byte_in[0],
+				byte_in[5] ^ byte_in[0],
+				byte_in[6] ^ byte_in[5] ^ byte_in[7] 	
+		    	  };
+
+endmodule 
