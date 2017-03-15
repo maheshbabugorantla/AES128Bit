061bb8e866d5ba62f214ab665c53a2ac21d399e7
Mahesh Babu Gorantla <mgorantl@purdue.edu>
2017-03-15 02:03:03 -0400

    KeySchedule working as expecting need to add more testcases to increase the converage

--- source/KeySchedule.sv
+++ source/KeySchedule.sv
@@ -8,52 +8,84 @@
 
 module KeySchedule
 (
-	input wire clk,
 	input wire [127:0] encryptKey,
-	output wire [1407:0] roundKeys,
-	output wire done
+	output wire [1407:0] roundKeys
 );
 		
-	reg [31:0] word4, word8, word12, word16, word20, word24, word28, word32, word36, word40; // Every Fourth Word
-	reg [31:0] word3, word7, word11, word15, word19, word23, word27, word31, word35, word39; // Every Third Word	
-	
-	assign roundKeys[127:0] = encryptKey; // Round Key 0
+	reg [127:0] roundKey0, roundKey1, roundKey2, roundKey3, roundKey4, roundKey5, roundKey6, roundKey7, roundKey8, roundKey9, roundKey10;  
+
+	assign roundKey0 = encryptKey;
 
 	// Round Key 1
-	assign word3 = encryptKey[127:96];
-	
-	reg [31:0] word14, word5, word6; 
-
-	g_func_key_expansion G_FUNC_1 (
-					 .inputWord(word3),
-					 .count(4'b0001),
-					 .outputWord(word4)
-				      );
-
-	assign word14 = word4 ^ encryptKey[31:0]; // XOR with First Word of Zeroeth Round and g(word3)
-	assign word5 = word14 ^ encryptKey[63:32];
-	assign word6 = word5 ^ encryptKey[95:64];
-	assign word7 = word6 ^ word3;
+	roundKey ROUND_KEY_1(
+				.inputKey(roundKey0),
+			      	.count(4'b0000),
+			      	.outputRoundKey(roundKey1)
+			    );
 
 	// Round Key 2
-	
+	roundKey ROUND_KEY_2(
+				.inputKey(roundKey1),
+			      	.count(4'b0001),
+			      	.outputRoundKey(roundKey2)
+			    );
+
 	// Round Key 3
+	roundKey ROUND_KEY_3(
+				.inputKey(roundKey2),
+			      	.count(4'b0010),
+			      	.outputRoundKey(roundKey3)
+			    );
 
 	// Round Key 4
+	roundKey ROUND_KEY_4( 
+				.inputKey(roundKey3),
+			      	.count(4'b0011),
+			      	.outputRoundKey(roundKey4)
+			    );
 
 	// Round Key 5
+	roundKey ROUND_KEY_5( 
+				.inputKey(roundKey4),
+			      	.count(4'b0100),
+			      	.outputRoundKey(roundKey5)
+			    );
 
 	// Round Key 6
+	roundKey ROUND_KEY_6( 
+				.inputKey(roundKey5),
+			      	.count(4'b0101),
+			      	.outputRoundKey(roundKey6)
+			    );
 
 	// Round Key 7
+	roundKey ROUND_KEY_7( 	
+				.inputKey(roundKey6),
+			      	.count(4'b0110),
+			      	.outputRoundKey(roundKey7)
+			    );
 
 	// Round Key 8
+	roundKey ROUND_KEY_8( 
+				.inputKey(roundKey7),
+			      	.count(4'b0111),
+			      	.outputRoundKey(roundKey8)
+			    );
 
 	// Round Key 9
+	roundKey ROUND_KEY_9( 
+				.inputKey(roundKey8),
+			      	.count(4'b1000),
+			      	.outputRoundKey(roundKey9)
+			    );
 
 	// Round Key 10
+	roundKey ROUND_KEY_10( 
+				.inputKey(roundKey9),
+	                        .count(4'b1001),
+	                        .outputRoundKey(roundKey10)
+ 			     );
 
-	// This is a signal to the Encryptor Controller that the Key Schedule is done generating all the 11 round keys needed
-	assign done = 1;
-
+	assign roundKeys = { roundKey10, roundKey9, roundKey8, roundKey7, roundKey6, roundKey5, roundKey4, roundKey3, roundKey2, roundKey1, roundKey0 };
+	
 endmodule 
